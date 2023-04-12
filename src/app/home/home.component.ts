0a91cd09b286dda69921182324cce5acdce4fe09
Gisela Jiménez Herrera <gisela.jimenez@dedalus.eu>
2023-04-12 13:23:21 +0200

    quitado el botón (añadida funcionalidad del botón al select) y añadido el logo del Publisher al mostrar los juegos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../service/api.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,16 +13,21 @@ export class HomeComponent {
 
   queryTypes: string[] = ["games", "characters", "places", "monsters", "bosses", "items", "dungeons"]
 
+  private unsubscribe$ = new Subject<void>();
+
   constructor(private apiService: ApiService) {}
 
   option= ''
   public page!: number
 
-  ngOnInit(): void {
-  }
+  ngOnDestroy() {this.unsubscribe$.next() }
+
+  ngOnInit(): void {}
 
   addData() {
-    this.apiService.getData(this.option).subscribe(data => {
+    this.apiService.getData(this.option)
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe(data => {
       this.data = data.data
       console.log(this.data)
     })
